c635a19b4cb4e1a118727fe2e58b41944e1fd8a3
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-10-10 10:45:08 +0900

    change serve version

--- supabase/functions/get_film_data/index.ts
+++ supabase/functions/get_film_data/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.203.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.0'
 
 // Get the environment variables
@@ -35,7 +35,7 @@ interface SupabaseFilm extends Film {
   embedding: number[]
 }
 
-serve(async (req: any) => {
+serve(async (req) => {
   const year = new URLSearchParams(req.url).get('year')
 
   if (!year) {
